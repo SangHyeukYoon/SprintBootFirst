2f7f119e79b848db4baba98d2aa697f5729b8086
SHYoon <nyoonsh97@gmail.com>
2022-01-25 13:12:49 +0900

    Update test

--- src/main/java/com/yoon/springboot/pack/web/dto/IndexController.java
+++ src/main/java/com/yoon/springboot/pack/web/dto/IndexController.java
@@ -1,5 +1,7 @@
 package com.yoon.springboot.pack.web.dto;
 
+import com.yoon.springboot.pack.config.auth.LoginUser;
+import com.yoon.springboot.pack.config.auth.dto.SessionUser;
 import com.yoon.springboot.pack.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
